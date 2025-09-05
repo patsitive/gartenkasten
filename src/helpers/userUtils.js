06cef85b3860a8df49c14f1c13ab94de5ec0d4f1
Pat <3947121+patsitive@users.noreply.github.com>
2025-09-05 15:26:44 +1200

    Update userUtils.js

--- src/helpers/userUtils.js
+++ src/helpers/userUtils.js
@@ -51,7 +51,7 @@ function forestData(data) {
   const treeCounts = JSON.parse(JSON.stringify(noteLabels));
 
   const filteredNotes = data.collections.note.filter((n) => {
-    return !n.fileOrFolder.includes("notes/Slices/");
+    return !n.inputPath.includes("notes/Slices/");
   });
   
   const canvasTrees = data.collections.note.map((n) => {
